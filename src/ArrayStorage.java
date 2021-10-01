d7171aa49c002fd264ec31804d8d8fbac6967af2
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-10-01 22:34:05 +0300

    Implement field size in class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,14 +5,17 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, size(), null);
+        Arrays.fill(storage, 0, size - 1, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        if (size() < storage.length) {
-            storage[size()] = r;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
         } else {
             System.out.println("Storage is full");
         }
@@ -20,13 +23,14 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         int index = 0;
-        while (index < size()) {
-            if (uuid.equals(storage[index].uuid)) {
+        while (index < size) {
+            if (!storage[index].uuid.equals(uuid)) {
+                index++;
+            } else {
                 break;
             }
-            index++;
         }
-        if (index < size()) {
+        if (index < size) {
             return storage[index];
         } else {
             return null;
@@ -34,10 +38,11 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i] == get(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, size() - 1 - i);
-                storage[size() - 1] = null;
+                System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
+                storage[size - 1] = null;
+                size--;
             }
         }
     }
@@ -46,14 +51,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int storageSize = 0;
-        while (storageSize < storage.length && storage[storageSize] != null) {
-            storageSize++;
-        }
-        return storageSize;
+        return size;
     }
 }
