e867afc0fd67907c67e7ed3ef4dc279a1dff88d4
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-12-08 18:59:15 +0300

    Add equals and hashCode

--- src/com/basejava/webapp/MainArray.java
+++ src/com/basejava/webapp/MainArray.java
@@ -18,7 +18,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | update uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -41,7 +41,13 @@ public class MainArray {
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
-                case "delete":
+                case "update":
+                    r = new Resume();
+                    r.setUuid(uuid);
+                    ARRAY_STORAGE.update(r);
+                    printAll();
+                    break;
+                    case "delete":
                     ARRAY_STORAGE.delete(uuid);
                     printAll();
                     break;
