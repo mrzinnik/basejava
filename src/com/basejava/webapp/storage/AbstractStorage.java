f31d1820c95e18d342aead3b8e9f6d6e56c44b1b
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2022-02-18 14:14:11 +0300

    lesson07 logging

--- src/com/basejava/webapp/storage/AbstractStorage.java
+++ src/com/basejava/webapp/storage/AbstractStorage.java
@@ -6,31 +6,39 @@ import com.basejava.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     @Override
     public final void update(Resume r) {
+        LOG.info("update " + r);
         updateResume(r, checkResumeForExistence(r.getUuid(), true));
     }
 
     @Override
     public final void save(Resume r) {
+        LOG.info("save " + r);
         saveResume(r, checkResumeForExistence(r.getUuid(), false));
     }
 
     @Override
     public final Resume get(String uuid) {
+        LOG.info("get " + uuid);
         return getResume(checkResumeForExistence(uuid, true));
     }
 
     @Override
     public final void delete(String uuid) {
+        LOG.info("delete " + uuid);
         deleteResume(checkResumeForExistence(uuid, true));
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> listStorage = getStorageAsList();
         Collections.sort(listStorage);
         return listStorage;
@@ -41,8 +49,10 @@ public abstract class AbstractStorage<SK> implements Storage {
         boolean isExist = isResumeExist(searchKey);
         if (isExist ^ mustExist) {
             if (isExist) {
+                LOG.warning("Resume with uuid = " + uuid + " already exists");
                 throw new ExistStorageException(uuid);
             }
+            LOG.warning("Resume with uuid = " + uuid + " doesn't exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
